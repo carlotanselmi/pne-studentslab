56ffe3e95350323e5cac04449264f9e7a21851b3
carlotanselmi <c.anselmi.2023@alumnos.urjc.es>
2024-01-31 18:33:47 +0100

    31/01

---
+++ Hello/S03 3101/dna_count_file.py
@@ -0,0 +1,10 @@
+with open('dna.txt', 'r') as dna:
+    seq = ""
+    for line in dna:
+        seq += line.replace("\n", "")
+    print("Total length:", len(seq))
+    dictionary = {"A": 0, "C": 0, "T": 0, "G": 0}
+    for e in seq:
+        if e in dictionary:
+            dictionary[e] += 1
+    print(dictionary)
