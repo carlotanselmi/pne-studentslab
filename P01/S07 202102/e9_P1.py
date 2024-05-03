3bc3f4d904cb0822fd3dcee6c2581f70f9d659a3
carlotanselmi <c.anselmi.2023@alumnos.urjc.es>
2024-05-03 18:16:59 +0200

    updating

--- P01/S07 202102/e9_P1.py
+++ P01/S07 202102/e9_P1.py
@@ -1,8 +1,7 @@
 from Seq1 import *
 print("-----| Practice 1, Exercise 9 |------")
 s = Seq()
-s.read_fasta("Genome Sequences/U5.fa")
-# "/home/alumnos/carlotaa/PycharmProjects/pne-studentslab/P00/Genome Sequences/U5.fa"
+s.read_fasta("../Genome Sequences/U5.fa")
 print(f"Sequence: (Length: {s.len()}) {s}")
 print(f"Bases: {s.count()}")
 print(f"Reverse: {s.reverse()}")
--- S08 2702/e3_08.py
+++ S08 2702/e3_08.py
@@ -2,15 +2,16 @@ import socket
 
 # SERVER IP, PORT
 PORT = 8081
-IP = "212.128.255.84" # depends on the computer the server is running
+IP = "127.0.0.1"  # depends on the computer the server is running
 
 while True:
     # -- Ask the user for the message
-
+    msg_input = input("Enter your message: ")
     # -- Create the socket
-
+    ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # -- Establish the connection to the Server
-
+    ls.connect((IP, PORT))
     # -- Send the user message
-
+    ls.send(str.encode(msg_input))
     # -- Close the socket
+    ls.close()
